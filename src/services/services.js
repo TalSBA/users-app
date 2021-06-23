3d7ef5a219f3046137f7f6ed307b2b6f7b500dec
TalSBA <60568087+TalSBA@users.noreply.github.com>
2021-06-23 14:16:27 +0300

    add home + table + form

---
+++ src/services/services.js
@@ -0,0 +1,26 @@
+const users = [
+    {
+        "FullName": "Jon Doe",
+        "DateOfBidrh": "26/09/1989",
+        "ID": "230496504"
+    },
+    {
+        "FullName": "Jon Doe",
+        "DateOfBidrh": "26/09/1989",
+        "ID": "230496504"
+    },
+    {
+        "FullName": "Jon Doe",
+        "DateOfBidrh": "26/09/1989",
+        "ID": "230496504"
+    }
+
+]
+
+export function getUsers() {
+    return users;
+}
+
+export function saveUser(user){
+
+}
